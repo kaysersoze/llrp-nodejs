04b388a3767d73273f66b5dce4bc360d84d2ac51
Piotr Zduniak <piotr@zduniak.net>
2018-05-22 13:29:28 +0200

    Typo correction

--- llrp/LLRPMain.js
+++ llrp/LLRPMain.js
@@ -215,7 +215,7 @@ var llrpMain = function (config) {
 							tag.epc = subParameters[parameterC.EPC96].toString('hex');
 						}
 						if (typeof subParameters[parameterC.EPCData] !== 'undefined') {
-							tag.epc = subParameters[parameterC.PCData].toString('hex');
+							tag.epc = subParameters[parameterC.EPCData].toString('hex');
 						}
 						if (typeof subParameters[parameterC.AntennaID] !== 'undefined') {
 							tag.antennaID = subParameters[parameterC.AntennaID].toString('hex');
@@ -330,3 +330,4 @@ var llrpMain = function (config) {
 llrpMain.prototype = new EventEmitter();
 
 module.exports = llrpMain;
+
